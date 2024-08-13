adcb3ea9d03238d24685d5ee50371058020c1d9c
Jagadeesh Musham <jagadeeshmusham@gmail.com>
2024-08-13 11:34:57 +0530

    Added appropriate place holder for each file

---
+++ Java_Practices/InterviewQuestions/amphora/engineering_test/src/main/java/com/amphora/engineering_test/App.java
@@ -0,0 +1,19 @@
+package InterviewQuestions.amphora.engineering_test.src.main.java.com.amphora.engineering_test;
+
+/**
+ * @author Admin This is main class which is used to start the application
+ */
+
+@SpringBootApplication
+public class App {
+	/**
+	 * The main method
+	 * 
+	 * @param args
+	 *            The arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		System.out.println("Hello Amphorian!");
+	}
+}
